b2a6a05f9dbd2608672c4c4f585db88115fefacf
William DeMeo <williamdemeo@gmail.com>
2013-12-18 21:36:07 -0500

    initial commit of missing scripts

---
+++ src/Matlab/GFIP/TrnsfFltrsScripts/convsdp.m
@@ -0,0 +1,53 @@
+function y=convsdp(u,v,gp)
+% Semidirect product convolution of 1-dimensional signals.
+%
+% INPUT
+%   u & 1D array (signal)
+%   v & 1D array (signal) of same length as u
+%   gp & (int) indicates group wrtw convolution is performed & default: 1 
+%
+% OUTPUT
+%   y & 1D array (signal) stores resulting convolution product.
+%
+% REMARKS
+%   o  The key operation which distinguishes the convolution
+%      \[        
+%             C(u)v = sum_x u(x) T(x) v
+%      \]
+%      over different groups is the translation $T(x) v$.
+%
+% See also: conv2sdp
+%
+% HISTORY
+% 2001.04.03 & Myoung An & wrote original program, named 
+%                          the file cn_2.m, but named the 
+%                          function contained in that file cnv2. 
+% 2004.03.29 & William DeMeo & added documentation,modified name 
+%                              of this file and name of some
+%                              functions called from this file.
+% 2004.04.29 & William DeMeo & minor mods
+%
+if nargin < 3, 
+  gp=1;    
+end;
+if length(u) ~= length(v), 
+  error('input signals must be of the same length'); 
+end;
+% TODO % accomodate signals of different length
+n2=size(u,1);             % signal length $2N$
+n=n2/2;                % $N$ : signal half-length
+m=n/2;                 % $N/2$ : signal quarter-length
+nn=(0:n-1);            % $\{0,1,\ldots,N-1\}$
+
+rn = indexset(n,gp);
+
+uu(1:n,1)=u(1:n,1);
+for k=1:n
+  uu(k+n,1)=u(rn(k)+n,1);
+end   
+[u1,u2]=n_2(uu(:,1),gp);           % was: sdpt(uu(:,1),gp);
+[v1,v2]=n_2(v(:,1),gp);            % was: sdpt(v(:,1),gp);
+c(1:n) = u2(1:n).*v2(1:n) + u2(1+n:n2).*v2(1+n:n2) + sqrt(2)*u1(1:n).*v1(1:n);
+c(1+n:n2) = u2(1+n:n2).*v2(1:n) + u2(1:n).*v2(1+n:n2) + sqrt(2)*u1(1+n:n2).*v1(1+n:n2);
+
+y=sqrt(n)*in_2(c,gp);                % was: sqrt(n)*isdpt(c,gp);
