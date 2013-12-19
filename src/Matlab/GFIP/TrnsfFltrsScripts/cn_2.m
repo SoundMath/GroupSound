baea3ce0266e9866b183d8363861f4d135963072
William DeMeo <williamdemeo@gmail.com>
2013-12-19 00:05:39 -0500

    changing some files back to Myoung's original versions, since mine seem broken

--- src/Matlab/GFIP/TrnsfFltrsScripts/cn_2.m
+++ src/Matlab/GFIP/TrnsfFltrsScripts/cn_2.m
@@ -1,51 +1,36 @@
-function y=cn_2(u,v,gp)
-% Semidirect product convolution of 1-dimensional signals.
-%
-% INPUT
-%   u & 1D array (signal)
-%   v & 1D array (signal) of same length as u
-%   gp & (int) indicates group wrtw convolution is performed & default: 1 
-%
-% OUTPUT
-%   y & 1D array (signal) stores resulting convolution product.
-%
-% REMARKS
-%   o  The key operation which distinguishes the convolution
-%      \[        
-%             C(u)v = sum_x u(x) T(x) v
-%      \]
-%      over different groups is the translation $T(x) v$.
-%
-% HISTORY
-% 2001.04.03 & Myoung An & wrote original program, named 
-%                          the file cn_2.m, but named the 
-%                          function contained in that file cnv2. 
-% 2004.03.29 & William DeMeo & added documentation,modified name 
-%                              of this file and name of some
-%                              functions called from this file.
-% 2004.04.29 & William DeMeo & minor mods
-%
-if nargin < 3, 
-  gp=1;    
-end;
-if length(u) ~= length(v), 
-  error('input signals must be of the same length'); 
-end;
-% TODO % accomodate signals of different length
-n2=size(u,1);             % signal length $2N$
-n=n2/2;                % $N$ : signal half-length
-m=n/2;                 % $N/2$ : signal quarter-length
-nn=(0:n-1);            % $\{0,1,\ldots,N-1\}$
-
-rn = indexset(n,gp);
-
+function y=cnv2(u,v,gp);
+[n2,tem]=size(u);
+n=n2/2;
+m=n/2;
+nn=linspace(0,n-1,n);
 uu(1:n,1)=u(1:n,1);
-for k=1:n
-  uu(k+n,1)=u(rn(k)+n,1);
-end   
-[u1,u2]=n_2(uu(:,1),gp);
-[v1,v2]=n_2(v(:,1),gp); 
-c(1:n) = u2(1:n).*v2(1:n) + u2(1+n:n2).*v2(1+n:n2) + sqrt(2)*u1(1:n).*v1(1:n);
-c(1+n:n2) = u2(1+n:n2).*v2(1:n) + u2(1:n).*v2(1+n:n2) + sqrt(2)*u1(1+n:n2).*v1(1+n:n2);
-
-y=sqrt(n)*in_2(c,gp);                % was: sqrt(n)*in_2(c,gp);
+if (gp==1) % x --> x^-1
+   rn=mod(n-nn,n)+1;
+   for k=1:n
+      uu(k+n,1)=u(rn(k)+n,1);
+   end   
+end
+%
+if (gp==2) % x^k --> x^(m+1)*k
+   rn=mod((m+1)*nn,n)+1;
+   for k=1:n
+      uu(k+n,1)=u(rn(k)+n,1);
+   end   
+end
+%
+if (gp==3) % x^k --> x^(m-1)*k
+   rn=mod((m-1)*nn,n)+1;
+   for k=1:n
+      uu(k+n,1)=u(rn(k)+n,1);
+   end   
+end
+%
+[d11,d12]=n_2(uu(:,1),gp);
+[d21,d22]=n_2(v(:,1),gp);
+c(1:n,1)=d12(1:n,1).*d22(1:n,1)+d12(1+n:n2).*d22(1+n:n2);
+c(1+n:n2,1)=d12(1+n:n2,1).*d22(1:n,1)+d12(1:n).*d22(1+n:n2);
+%
+c(1:n,1)=c(1:n,1)+sqrt(2)*d11(1:n,1).*d21(1:n,1);
+c(1+n:n2,1)=c(1+n:n2)+sqrt(2)*d11(1+n:n2,1).*d21(1+n:n2,1);
+%
+y=sqrt(n)*in_2(c,gp);
