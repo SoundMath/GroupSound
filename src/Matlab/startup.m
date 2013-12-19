2e55940367fe6289229d83912b4e6169c02d4401
William DeMeo <williamdemeo@gmail.com>
2013-12-19 18:25:23 -0500

    fixed problems with startup file

--- src/Matlab/startup.m
+++ src/Matlab/startup.m
@@ -1,6 +1,6 @@
 %  startup.m 
 %
-%  Put this file in ~/.octaverc  to initialize Octave path.
+%  Put this file in ~/.octaverc to initialize Octave path.
 %  Put this file in ~/matlab/startup.m to initialize Matlab path.
 %  
   disp('---------------------------------------------------');
@@ -18,39 +18,21 @@
   % (util)
   dir2add = fullfile(MATLABROOT, 'util');
   if(exist(dir2add)==7),
-    addpath(dir2add,1);
+    disp(sprintf('      %s', dir2add));  addpath(dir2add);
   end;
 
   % "Group Filters and Image Processing," by An & Tolimieri
   dir2add = fullfile(MATLABROOT, 'GFIP', 'TrnsfFltrsScripts');
-  if(exist(dir2add)==7), addpath(dir2add); end;
+  if(exist(dir2add)==7), 
+    disp(sprintf('      %s', dir2add));  addpath(dir2add);
+  end;
 
   dir2add = fullfile(MATLABROOT, 'GFIP', 'Documented');
-  if(exist(dir2add)==7), addpath(dir2add); end;
+  if(exist(dir2add)==7),
+    disp(sprintf('      %s', dir2add));  addpath(dir2add);
+  end;
 
 
   disp(' ');
   disp('_______________________GroupSound: v.0.01 2013.12.19__');
 
-  % The following is not being used now, but I left it in because 
-  % it shows an alternative way to add directories to the Octave path.
-  if 0,
-    dir2add = fullfile(Toolbox, 'stsa');
-    if(exist(dir2add)==7),
-      addpath( ...
-          [Toolbox filesep 'stsa'], ...
-          [Toolbox filesep 'stsa' filesep 'demos'], ...
-          [Toolbox filesep 'stsa' filesep 'display'], ...
-          [Toolbox filesep 'stsa' filesep 'eval'], ...
-          [Toolbox filesep 'stsa' filesep 'fileio'], ...
-          [Toolbox filesep 'stsa' filesep 'gaintabs'], ...
-          [Toolbox filesep 'stsa' filesep 'noise'], ...
-          [Toolbox filesep 'stsa' filesep 'pcptmdls'], ...
-          [Toolbox filesep 'stsa' filesep 'postproc'], ...
-          [Toolbox filesep 'stsa' filesep 'preproc'], ...
-          [Toolbox filesep 'stsa' filesep 'specmod'], ...
-          [Toolbox filesep 'stsa' filesep 'stft'], ...
-          [Toolbox filesep 'stsa' filesep 'suprules'], ...
-          [Toolbox filesep 'stsa' filesep 'suprules' filesep 'base']);
-    end;
-  end;
